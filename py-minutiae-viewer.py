dc1bcc6059795bb8e0dd4aacfdcc6c693e64801b
IgniparousTempest <cr.pitcher@gmail.com>
2017-08-24 15:39:38 +0200

    Removed unnecessary frame

--- py-minutiae-viewer.py
+++ py-minutiae-viewer.py
@@ -1,20 +1,4 @@
-import platform
-
-from pathlib import Path
-from tkinter import PhotoImage
-from ttkthemes import ThemedTk
-
 from pyminutiaeviewer import gui
 
-root = ThemedTk()
-if platform.system() is 'Windows':
-    root.set_theme("vista")
-else:
-    root.set_theme("clearlooks")
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
-root.title("Py Minutiae Viewer")
-img = PhotoImage(file=Path(__file__).resolve().parent / 'pyminutiaeviewer' / 'images' / 'icon.png')
-root.iconphoto(True, img)
-app = gui.Root(root)
-app.mainloop()
+root = gui.Root()
+root.mainloop()
--- pyminutiaeviewer/gui.py
+++ pyminutiaeviewer/gui.py
@@ -1,15 +1,27 @@
-from tkinter import NSEW
-from tkinter.ttk import Frame, Style, Notebook
+import platform
+from pathlib import Path
+from tkinter import NSEW, PhotoImage
+from tkinter.ttk import Notebook, Frame
+
+from ttkthemes import ThemedTk
 
 from pyminutiaeviewer.gui_editor import MinutiaeEditorFrame
 
 
-class Root(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
+class Root(ThemedTk):
+    def __init__(self):
+        ThemedTk.__init__(self)
+        if platform.system() is 'Windows':
+            self.set_theme("vista")
+        else:
+            self.set_theme("clearlooks")
+
+        self.title("Py Minutiae Viewer")
+        img = PhotoImage(file=Path(__file__).resolve().parent / 'images' / 'icon.png')
+        self.iconphoto(True, img)
+
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        self.grid(sticky=NSEW)
 
         self.notebook = Notebook(self)
         self.notebook.grid(row=0, column=0, sticky=NSEW)
